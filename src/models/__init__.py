8372c79767dd04e5d099f761eb9b87d778b23019
Einfach Deutsch Builder <dev@einfach-deutsch.local>
2026-07-12 16:39:10 +0530

    Avoid importing lora_trainer in models package init to silence local venv warnings

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,6 @@
 """Model training and inference components."""
 
 from src.models.baseline_trainer import train
-from src.models.lora_trainer import train as train_lora
 from src.models.simplifier import Simplifier
 
-__all__ = ["train", "train_lora", "Simplifier"]
+__all__ = ["train", "Simplifier"]
